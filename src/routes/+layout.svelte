e57f7259e3b57ffd9693788499ecf3ccdd769f5e
oliverrr123 <oliver.cingl@gmail.com>
2025-08-29 22:08:06 -0700

    feat: new content for main page + content for growbyte page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
     {@render children()}
     <footer>
         <div class="flex justify-center gap-4 pt-16">
+            <a href="https://x.com/olivercingl/" target="_blank" aria-label="X profile"><img src="/icons/x.svg" alt="Link to my X" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
             <a href="https://github.com/oliverrr123/" target="_blank" aria-label="Github profile"><img src="/icons/github.svg" alt="Link to my GitHub" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
             <a href="https://linkedin.com/in/olivercingl/" target="_blank" aria-label="Linkedin profile"><img src="/icons/linkedin.svg" alt="Link to my LinkedIn" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
             <a href="mailto:oliver.cingl@gmail.com" target="_blank" aria-label="Email"><img src="/icons/mail.svg" alt="Link to my email" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
---
+++ src/routes/growbyte/+page.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+    let websites: { title: string; description: string; image: string; alt: string; link: string; }[] = [
+        {
+            title: 'Real Estate',
+            description: 'Showcase website of a real estate project.',
+            image: 'client1.png',
+            alt: 'Screenshot of a website I built for Growbyte company',
+            link: 'https://modricka.realyx.cz/'
+        },
+        {
+            title: 'Finance Company',
+            description: 'Portfolio website of a finance company specializing in receivables.',
+            image: 'client2.png',
+            alt: 'Screenshot of a website I built for Luma production agency',
+            link: 'https://expohledavky.cz/'
+        },
+        {
+            title: 'Sales Funnel',
+            description: 'Sales funnel for a personal growth course.',
+            image: 'client3.png',
+            alt: 'Screenshot of a website I built for SX Media',
+            link: 'https://konzultace.martinkrejcir.cz/'
+        }
+    ]
+    // let tools: { name: string; image: string; }[] = [
+    //     {
+    //         name: 'My Brain',
+    //         image: 'brain.png'
+    //     },
+    //     {
+    //         name: 'Next.js',
+    //         image: 'nextjs.svg'
+    //     },
+    //     {
+    //         name: 'Svelte',
+    //         image: 'svelte.svg'
+    //     },
+    //     {
+    //         name: 'TypeScript',
+    //         image: 'typescript.svg'
+    //     },
+    //     {
+    //         name: 'Tailwind',
+    //         image: 'tailwind.svg'
+    //     },
+    //     {
+    //         name: 'Supabase',
+    //         image: 'supabase.svg'
+    //     },
+    //     {
+    //         name: 'PostgreSQL',
+    //         image: 'postgres.svg'
+    //     },
+    //     {
+    //         name: 'Python',
+    //         image: 'python.svg'
+    //     },
+    //     {
+    //         name: 'GitHub',
+    //         image: 'github.svg'
+    //     },
+    //     {
+    //         name: 'Cursor',
+    //         image: 'cursor.webp'
+    //     },
+    //     {
+    //         name: 'Claude Code',
+    //         image: 'claude.svg'
+    //     }
+    // ]
+</script>
+
+
+<svelte:head>
+    <title>Oliver Cingl | GrowByte</title>
+</svelte:head>
+
+<a href="/" class="text-white text-sm text-opacity-50 underline pt-16 block">{'<'}&nbsp; Back to main page</a>
+<h1 class="text-3xl font-bold pb-8 pt-2">GrowByte</h1>
+<h3 class="text-white text-opacity-80 text-lg font-light">We make all kinds of websites and web apps, whether it's a simple one-page portfolio or a complex custom CRM.<br><br>We also MVPs and custom AI solutions to automate your business processes.<br><br>For most of our clients, we made a system for automatic email cold outreach. It automatically generates leads and sends them a personalized emails with followups.<br><br>Below are some examples of websites we built, to see more, including AI automations, contact me using the links on the bottom of the page.</h3>
+<h2 class="text-xl font-semibold pt-20 pb-6 text-white text-opacity-90">Websites we built:</h2>
+<section class="w-full flex flex-col justify-center gap-20">
+    {#each websites as website}
+        <div class="flex flex-col gap-2">
+            <img src="/images/growbyte/{website.image}" alt={website.alt} class="w-full rounded-xl">
+            <h2 class="font-bold text-lg mt-2">{website.title}</h2>
+            <p class="text-white text-opacity-75 pb-1">{website.description}</p>
+            <a href={website.link} target="_blank"><button class="font-semibold bg-white text-[#181818] rounded-lg px-4 py-1.5">See website</button></a>
+        </div>
+    {/each}
+</section>
+<!-- <h2 class="text-2xl font-semibold pt-20 pb-6 text-white text-opacity-90">Tools I use:</h2>
+{#each tools as tool}
+    <div class="flex gap-1 items-center">
+        <img src="/icons/growbyte/{tool.image}" alt="{tool.name} icon" class="w-12 h-12 p-2">
+        <p>{tool.name}</p>
+    </div>
+{/each} -->
