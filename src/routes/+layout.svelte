e85a5fe6b04d4f8ea7ddec202407fa4e5fc511a2
oliverrr123 <oliver.cingl@gmail.com>
2026-05-22 10:50:12 +0200

    feat: new pfp + fix myfriend link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { injectAnalytics } from '@vercel/analytics/sveltekit'
 
     let { children } = $props();
+    const year = new Date().getFullYear();
 </script>
 
 <main class="bg-[#181818] font-Montserrat text-white px-10 sm:px-[15vw] md:px-[20vw] lg:px-[25vw] xl:px-[33vw]">
@@ -14,6 +15,6 @@
             <a href="https://linkedin.com/in/olivercingl/" target="_blank" aria-label="Linkedin profile"><img src="/icons/linkedin.svg" alt="Link to my LinkedIn" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
             <a href="mailto:oliver.cingl@gmail.com" target="_blank" aria-label="Email"><img src="/icons/mail.svg" alt="Link to my email" class="w-8 h-8 opacity-75 hover:opacity-100 transition-opacity"></a>
         </div>
-        <h6 class="py-6 text-center">© 2025 Oliver Cingl</h6>
+        <h6 class="py-6 text-center">© {year} Oliver Cingl</h6>
     </footer>
 </main>
--- src/routes/myfriend/+page.svelte
+++ src/routes/myfriend/+page.svelte
@@ -40,4 +40,4 @@
 <p class="text-white text-opacity-80 text-lg font-light">In the United States, 16.2 million seniors live completely alone. That's more people than the entire population of several U.S. states combined. Many of them rarely see their family, or not at all, and loneliness becomes part of their daily life.</p>
 <br>
 <p class="text-white text-opacity-80 text-lg font-light">That's why I created MyFriend.</p>
-<a href="https://myfriend.growbyte.co/" target="_blank" class="text-xl font-semibold text-white text-opacity-90 block underline-offset-4 pt-20 pb-20"><span class="inline-flex gap-3 items-center border-b w-auto">More info on the website <img src="/icons/link.svg" alt="Link to my media production agency website" class="w-5 h-5"></span></a>
+<a href="https://growbyte.co/myfriend" target="_blank" class="text-xl font-semibold text-white text-opacity-90 block underline-offset-4 pt-20 pb-20"><span class="inline-flex gap-3 items-center border-b w-auto">More info on the website <img src="/icons/link.svg" alt="Link to my media production agency website" class="w-5 h-5"></span></a>
